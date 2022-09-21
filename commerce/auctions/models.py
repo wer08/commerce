8d34e963c36d4d8c9cdbcb27db093a66d7fca65b
wer08 <wz08061992@gmail.com>
2022-09-21 16:29:50 +0200

    Working on comment and bidding

--- commerce/auctions/models.py
+++ commerce/auctions/models.py
@@ -1,5 +1,9 @@
+from urllib import request
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+from crum import get_current_user
+
+
 
 
 class User(AbstractUser):
@@ -21,20 +25,30 @@ class AuctionListing(models.Model):
     description = models.CharField(max_length=512)
     starting_bid = models.DecimalField(max_digits=10, decimal_places=2)
     current_price = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
     image = models.CharField(max_length=10240,default="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRqEWgS0uxxEYJ0PsOb2OgwyWvC0Gjp8NUdPw&usqp=CAU",blank=True,null=True)
 
     def __str__(self):
-        return f"ID: {self.id} Title: {self.title}, decription: {self.description}, current price: {self.current_price}"
+        return f"Title: {self.title}, decription: {self.description}, current price: {self.current_price}"
     
     def save(self, *args, **kwargs):
-        self.current_price = self.starting_bid
+        if self.current_price == None:
+            self.current_price = self.starting_bid
+        self.owner = get_current_user()
         super().save(*args, **kwargs)
 
 class Bid(models.Model):
     listing_id = models.ForeignKey(AuctionListing, on_delete=models.CASCADE)
-    bid = models.IntegerField()
+    bid = models.DecimalField(max_digits=10, decimal_places=2)
     date = models.DateField(auto_now_add=True)
     bidder = models.ForeignKey(User, on_delete=models.CASCADE)
+    
+    def save(self, *args, **kwargs):    
+        self.bidder = get_current_user()
+        super().save(*args, **kwargs)
+    
+    def __str__(self):
+        return f"{self.bidder} bid : {self.bid} on : {self.listing_id}"
 
 class Watchlist(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
--- commerce/auctions/views.py
+++ commerce/auctions/views.py
@@ -4,17 +4,37 @@ from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 from django.shortcuts import redirect
 from django.urls import reverse
+from django import forms
 from django.forms import ModelForm
+from decimal import Decimal
 from .models import User, AuctionListing, Watchlist, Comment, Bid
 from operator import itemgetter
-
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 
 class NewListingForm(ModelForm):
     class Meta:
         model = AuctionListing
         fields = ["title", "category", "description", "starting_bid", "image"]
 
+class NewBidForm(forms.Form):
+    bid = forms.DecimalField() 
+
+class NewCommentForm(forms.Form):
+    comment = forms.CharField(widget=forms.Textarea, label="")
+
+def checkWatchlist(request,listing):
+    if request.user.is_authenticated:
+        if Watchlist.objects.filter(listing = listing, user = request.user).exists():
+            flag = True
+        else:
+            flag=False
+    else:
+        flag=False
+    return flag
     
+    
+
 
 def newListing(request):
     if request.method == "POST":
@@ -44,30 +64,63 @@ def watchlist(request):
     for object in list:
         listings.append(object.listing)
 
-
-
     return render(request, "auctions/watchlist.html",{
         "listings": listings
     })
 
+def addComment(request):
+    if request.method == "POST":
+        form2 = NewCommentForm(request.POST)
+        if form2.is_valid():
+            print("I'm here")
+            comment = form2.cleaned_data["comment"]
+            comment_object = Comment(comment = comment, listing = listing_object, user = request.user)
+            comment_object.save()
+    return render(request, "auctions/listing.html", {
+    "flag": flag,
+    "listing": listing_object,
+    "form": NewBidForm(initial={"bid": listing_object.current_price + round(Decimal(0.10),2)})
+    })
+
+
 def listing(request, listing):
+    listing_object = AuctionListing.objects.get(id = int(listing))
+    
     if request.method == "POST":
         listing_object = AuctionListing.objects.get(id = int(listing))
-        new_object = Watchlist(listing = listing_object, user = request.user)
         if flag:
+            new_object = Watchlist.objects.get(listing = listing_object, user = request.user)
             new_object.delete()
+            return redirect("index")
         else:
+            new_object = Watchlist(listing = listing_object, user = request.user)
             new_object.save()
-        return redirect("watchlist")
-    
-    listing_object = AuctionListing.objects.get(id = int(listing))
-    if listing_object.exists():
-        flag = True
-    else:
-        flag=False
+            flag = True
+
+        form = NewBidForm(request.POST)
+        if form.is_valid():
+            bid = form.cleaned_data['bid']
+            if bid > listing_object.current_price:
+                bid_object = Bid(bid = bid, listing_id = listing_object)
+                bid_object.save()
+                listing_object.current_price = bid_object.bid
+                listing_object.save()
+                messages.add_message(request, messages.SUCCESS, 'Your bid was succesful')
+                return redirect("index")
+            else:
+                messages.add_message(request, messages.WARNING, 'Bid must be higher than current price')
+                return render(request, "auctions/listing.html", {
+                    "flag": flag,
+                    "listing": listing_object,
+                    "form": NewBidForm(initial={"bid": listing_object.current_price + round(Decimal(0.10),2)})
+                })
+        
+        
+
     return render(request, "auctions/listing.html", {
         "flag": flag,
-        "listing": listing_object
+        "listing": listing_object,
+        "form": NewBidForm(initial={"bid": listing_object.current_price + round(Decimal(0.10),2)})
     })
 
 
